f499462df76d428fbf95cbd3f5ea06baf86f6fbb
waxhsu <98721786+waxhsu@users.noreply.github.com>
2022-10-10 09:49:41 -0500

    class13 upload

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,19 +1,17 @@
-document.getElementById('purple').onmouseenter = partyPurple
-document.getElementById('green').onmouseenter = partyGreen
+document.getElementById('purple').mouseenter = partyPurple
+document.getElementByID('green').mouseenter = partyGreen
 
-function partyPurple() {
+function partyPurple (){
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
   document.querySelector('body').style.color = 'white'
 }
 
-function partyGreen() {
+function partyGreen(partyPurple) {
   document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
   document.querySelector('body').style.color = 'white'
 }
 
-
-
-//purple rgba(241,63,247,1);
-//green rgba(0,253,81,1);
+//purple rgba(241,63,247,1)
+//green rgba(0,253,81,1)
 //blue rgba(0,254,255);
 //grey rgba(50,50,50);
