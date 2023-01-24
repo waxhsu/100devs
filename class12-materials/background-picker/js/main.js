941589b1de90627b62b0714b45136d7afd20c328
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-01-24 12:50:22 -0500

    Update main.js

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -16,16 +16,18 @@
 //blue rgba(0,254,255)
 //grey rgba(50,50,50);
 
+document.querySelector('#purple').onclick = partyPurple
+document.querySelector('#green').onmouseenter = partyGreen
 
-document.querySelector('#purple').onmouseenter = partyPurp
-
-function partyPurp() {
+function partyPurple() {
     document.querySelector('body').style.backgroundColor = 'goldenrod'
     document.querySelector('body').style.color = 'white'
-    document.querySelector('h2').innerText = 'I changed it'
-    
+    document.querySelector('h2').innerText = 'I changed this text'
 }
 
-
-
+function partyGreen() {
+    document.querySelector('body').style.backgroundColor = 'navy'
+    document.querySelector('body').style.color = 'goldenrod'
+    document.querySelector('h2').innerText = 'mouseenter change'
+}
 
