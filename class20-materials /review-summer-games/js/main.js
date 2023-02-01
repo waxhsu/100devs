8e3287590f6059b38729bf0f7890664e0695643d
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-01 11:01:27 -0500

    practice

--- class20-materials /review-summer-games/js/main.js
+++ class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,24 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+// THIS IS ALSO CORRECT
+function mul(arr) {
+    let product = 1
+    for(let i = 0; i < arr.length; i++){
+        product *= arr[i]
+        console.log(product)
+    }
+}
+
+mul([10,2,3])
+
+
+// OTHER SOLUTION
+function multiNum(arr){
+    let product = 1
+    arr.forEach(num => product *= num)
+    console.log(product)
+}
+
+multiNum([10,2,3])
+
+// arr and num are parameters; therefore it does not need a declaration
