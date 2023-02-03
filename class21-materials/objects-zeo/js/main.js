a23627afa29fec0149485f5fe05036a051f097c4
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-03 10:21:35 -0500

    daily practice

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -29,5 +29,9 @@ let stopwatch = {}
 
 
 
+    const areYouPlayingBanjo = name => name + (name[0].toLowerCase() === 'r' ? ' plays' : ' does not play') + ' banjo'
 
 
+// function areYouPlayingBanjo(name) {
+//   return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+// }
