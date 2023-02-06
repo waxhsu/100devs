6320e25add9e08faa5724b97a3e7c88a9fb5b4b5
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-06 14:23:01 -0500

    review review

--- class23-materials/space-review/js/main.js
+++ class23-materials/space-review/js/main.js
@@ -2,11 +2,50 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+newArr = [1,2,3,4]
+let sum = 0
+for (let i = 0; i < newArr.length; i++)
+sum += newArr[i]
+console.log(sum)
+
+// LEON EXAMPLE
+// let nums = [10,20,30,50]
+// console.log(nums.reduce((acc, c ) => acc + c, 0))
+
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+
+function takeArr (arr) {
+    for (let i = 0; i < arr.length; i++)
+    console.log (Math.pow(arr[i],arr[i]))
+}
+console.log(takeArr([1,2,3,4,5]))
+
+
+//LEON EXAMPLE
+// let newArr = nums => nums.map( num => num**2)
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+const str1 = 'RACECAR'
+let newArr1 = str1.split('').reverse().join('')
+console.log(newArr1)
+
+
+
+function reverseStr (newArr1) {
+    console.log(newArr1.toString())
+}
+
+// LEON EXAMPLE
+let unoReverse = str => console.log(str.split('').reverse().join(''))
+
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+// can check if normal and reversed are the same
+
+
+// const palindromeCheck = str => console.log(str === str.split('').reverse().join(''))
--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -1,13 +1,54 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
 
+let str = 'ruxin is a doggo?'
+
+const qOrNah = _ => str.includes('?') ? 'it is a question' : 'it is not a question'
+
+console.log(qOrNah(str))
+
+// console.log(str.endsWith('?'))
+
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+
+let str1 = 'george minute jr. dev recent four gall bladder'
+
+console.log(str1.replaceAll('jr. dev', 'software engineer'))
+//FUCK IDK
+
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
 
+function rockPS() {
+    if (Math.random < .33) {
+        return 'rock'
+    } else if (Math.random < .66) {
+        return 'paper'
+    } else {
+        return 'scissor'
+    }
+}
+
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
 
+function checkWin(choice) {
+    if (choice.toLowerCase === 'rock' && rockPS() === 'paper' || choice.toLowerCase === 'paper' && rockPS() === 'scissor' || choice.toLowerCase === 'scissor' && rockPS() === 'rock') {
+        console.log('you loooooooooooose')
+    } else if (choice.toLowerCase === rockPS()) {
+        console.log('titietieiteietieititeititties')
+    } else {
+        console.log('you r wiener')
+    }
+}
+
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+
+function playerInput(arr){
+    arr.forEach(choice => checkWin(choice))
+}
+
+
+
