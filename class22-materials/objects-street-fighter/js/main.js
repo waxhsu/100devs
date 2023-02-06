85b2cdbca120bd1ff2e2cb805cf175a0ca90ae05
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-06 11:55:05 -0500

    Update main.js

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,14 +1,25 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
 
-function MakeSFChar (charGender, charStyle, charHealth, charATK, charDEF, charHeight) {
-    this.style = charStyle
-    this.health = charHealth
-    this.attack = charATK
-    this.defense = charDEF
-    this.height = charHeight
-
-    function 
+function MakeSmashCharacter (finalSmash, neutralB, upB, downB, sideB, catchPhrase) {
+    this.neutralB = neutralB
+    this.upB = upB
+    this.downB = downB
+    this.sideB = sideB
+    this.finalSmash = finalSmash
+    this.catchPhrase = catchPhrase
+    
+    this.taunt = function() {
+        console.log(`HEY CMON CMON ${this.finalSmash}`)
+    }
+    this.winning = function() {
+        console.log(`HEY ${this.catchPhrase}`)
+    }
+    this.dash = function() {
+        console.log(`PUNK CHECK`)
+    }
 } 
 
+let terryBogard = new MakeSmashCharacter('BUSTA WHOALF', 'POWA WAYV', 'RISING TAKO', 'POWA DAHHNK', 'BAHHN KNAKLE', 'R U OK?????')
 
+let marioMario = new MakeSmashCharacter('WAHOOOOO', 'fireball', 'money maker', 'F.L.U.D.D.', 'cape', 'ITSA ME, MAAHRIO')
