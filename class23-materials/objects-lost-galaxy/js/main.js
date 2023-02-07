7aaa580b41809d1392a70a99535bf613229c291c
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-07 09:36:24 -0500

    daily practice

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
 //Create a dog object that has four properties and three methods
+
+
+
+function Sk8r (skateboard, helmetBrand, helmetColor, style) {
+    this.skateboard = skateboard
+    this.helmetBrand = helmetBrand
+    this.helmetColor = helmetColor
+    this.sk8Style = style
+
+    this.win = function() {
+        console.log(`my style of ${style} is best style`)
+    }
+
+    this.lose = function() {
+        console.log(`i lose because ${skateboard} was broke`)
+    }
+
+    this.salty = function() {
+        console.log(`i know how to use excel spreadsheet with my ${this.helmetBrand}`)
+    }
+}
+
+
+
