1890dba59c7189baf110d2190e3e8c7756452187
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-08 10:55:11 -0500

    daily practice123

--- class23-materials/objects-dog-photos/js/main.js
+++ class23-materials/objects-dog-photos/js/main.js
@@ -1,3 +1,37 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
 
 
+
+
+
+function rockPaperScissors(){
+    if (Math.random() > .66) {
+        return 'rock'
+    } else if (Math.random() > .33) {
+        return 'paper'
+    } else {
+        return 'scissor'
+    }
+}
+
+
+function checkWin(choice){
+    let botChoice = rockPaperScissors()
+    if (choice == 'rock' && botChoice == 'paper' || choice == 'paper' && botChoice == 'scissors' || choice == 'scissors' && botChoice == 'rock') {
+        console.log('you lOOOoooOOoose')
+    } else if (choice == botChoice) {
+        console.log('tieteiteititsahjdfhkal;sdfjkls;afjdkls;aeiet')        
+    } else {
+        console.log('WIN ying success great')
+    }
+}
+
+function timesPlayed(playerInput) {
+    playerInput.forEach(choice => checkWin(choice))
+
+    // for (let i = 0; i<playerInput.length; i++)
+    // checkWin(playerInput[i])
+
+}
+
+timesPlayed(['rock', 'paper', 'paper'])
