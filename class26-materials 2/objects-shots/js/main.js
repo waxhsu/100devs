5765290c01db85b246c7c2a7ed692698eb8cf5d4
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-09 14:58:39 -0500

    cocktail API

--- class26-materials 2/objects-shots/js/main.js
+++ class26-materials 2/objects-shots/js/main.js
@@ -1,20 +1,49 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+// document.querySelector('button').addEventListener('click', getDrink)
+
+// function getDrink(){
+//   let drink = document.querySelector('input').value
+//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//       let min = 0
+//       let max = [data.drinks.length-1]
+//       let randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+//        // Math.floor((Math.random()*10))
+//       console.log(data.drinks[randomNumber]) // VIEW OBJECT INFO
+// /*       document.querySelector('h2').innerText = data.drinks[randomNumber].strDrink
+//       document.querySelector('img').src = data.drinks[randomNumber].strDrinkThumb
+//       document.querySelector('h3').innerText = data.drinks[randomNumber].strInstructions
+//       document.querySelector('h4').innerText = data.drinks[randomNumber].strIngredient1 */
+
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     });
+// }
+
+
+
+
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
-    let drink = document.querySelector('input').value
-
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+  let drink = document.querySelector('input').value
+  let randomNumber = Math.floor((Math.random()*10))
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.drinks)
-      document.querySelector('h2').innerText = data.drinks[0].strDrink // array of objects, we are getting the first object
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions
+      let min = 0
+      let max = [data.drinks.length-1]
+      let randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+      console.log(data.drinks[randomNumber]) 
+        document.querySelector('h2').innerText = data.drinks[randomNumber].strDrink
+        document.querySelector('img').src = data.drinks[randomNumber].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[randomNumber].strInstructions
+        document.querySelector('h4').innerText = data.drinks[randomNumber].strIngredient1
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
-
 }
-
