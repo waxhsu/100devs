3e818cba7246d3d9b6285b628b89ad86d417d012
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-20 13:30:01 -0500

    learned inheritance and parent-child syntaxes

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,43 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, breed){
+        super(name);
+        this.breed = breed
+    }
+}
+
+
+
+
+class MakeSmashChar {
+    constructor(name, normalAtk){
+        this.name = name
+        this.atk = normalAtk
+    }
+    announcer(){
+        console.log.toUpperCase((`${this.name}`))
+    }
+    punch(){
+        console.log(`${this.atk} hehe haha lol`)
+    }
+}
+
+class Series extends MakeSmashChar{
+    constructor (name, normalAtk, series){
+        super(name, normalAtk)
+        this.series = series
+    }
+    origin(){
+        console.log(`${this.name} is from ${this.series}`)
+    }
+}
