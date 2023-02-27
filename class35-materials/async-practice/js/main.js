1f267c37615a058a5f8396889738cf6c56d2cb05
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-02-27 14:11:46 -0500

    Update main.js

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -135,10 +135,16 @@
 // getPaid()
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)
+}
+getACuteDogPhoto()
 
+try {
+    console.log('lol');
+}
+catch (err) {
+    console.log('there is an error')
+}
